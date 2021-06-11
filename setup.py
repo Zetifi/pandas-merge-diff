207f7dc3ea5dd17f0122a706495ca9c30b986167
Ben Letchford <ben@zetifi.com>
2021-06-11 18:43:54 +1000

    Fix typo in setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,11 @@ import setuptools
 
 setup(
     name="pandas-merge-diff",
-    version="0.1,
+    version="0.1",
     description="",
     author="Ben Letchford",
     url="https://github.com/Zetifi/pandas-merge-diff",
-    install_requires=[
-        "pandas"
-    ],
+    install_requires=["pandas"],
     packages=setuptools.find_packages(),
     python_requires=">=3.7",
 )
